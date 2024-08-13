ec84ba31f1047cc9e372d372ec2e5dcf5acca9f0
Matt <73149745+Deathbringer98@users.noreply.github.com>
2024-08-13 16:30:27 -0230

    Delete Final Sprint Front End S4 directory

--- Final Sprint Front End S4/server.js
+++
@@ -1,6 +0,0 @@
-const app = require('./app'); // Import the express application
-const PORT = process.env.PORT || 3000; // Define the port
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
